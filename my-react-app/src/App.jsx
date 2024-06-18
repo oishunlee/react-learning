7bec4b8546445e8ac1cd7bbc429506082e09e47d
Carol Lee <oishun@gmail.com>
2024-06-18 20:00:01 +1000

    add props and propstype

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,19 @@
 import Card from './Card.jsx'
 import Button from './Button.jsx'
-
+import Student from './Student.jsx'
 
 function App() {
   return(
-
     <>
     <Card/>
-    
+    <Student name="Charlotte" age={32} isStudent={true}/>
+    <Student name="Mushie" age={2.2} isStudent={true}/>
+    <Student name="Captain" age={5} isStudent={false}/>
+    <Student/>
+    <Student name="Larry"/>
     <Button/>
     </>
-    
   );
-
-  
 }
 
 export default App
